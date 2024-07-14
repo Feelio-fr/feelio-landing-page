e3b76543b6131ce666b4a6bbcc655940bbcbc4a0
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-07-14 20:36:47 +0200

    ✨ feat: Animation of swing and run on scroll (V3)

--- src/components/runningMan/RunningMan.tsx
+++ src/components/runningMan/RunningMan.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { motion, useViewportScroll, useTransform, useScroll } from 'framer-motion';
+import { motion, useTransform, useScroll } from 'framer-motion';
 import styles from './RunningMan.module.css';
 import bodyImage from '../../assets/runningGuy/body2.png';
 import leftLegImage from '../../assets/runningGuy/leftLeg2.png';
@@ -8,7 +8,6 @@ import rightLegImage from '../../assets/runningGuy/rightLeg2.png';
 import rightArmImage from '../../assets/runningGuy/rightArm.png'; 
 
 const RunningMan = () => {
-
     const refGuy = useRef(null);
     const { scrollYProgress } = useScroll({
         target: refGuy,
@@ -25,24 +24,57 @@ const RunningMan = () => {
         return () => window.removeEventListener('resize', handleResize)
     }, [])
     
-    const scrollTransformX = useTransform(scrollYProgress, [0, 1], [windowSize / 2 +50, -windowSize / 2 -50]);
+    const scrollTransformX = useTransform(scrollYProgress, [0, 1], [windowSize / 2 , -windowSize / 2 - 150]);
+    // Create transformations for 20 rotations
+    const armSwing = useTransform(scrollYProgress, 
+        Array.from({ length: 21 }, (_, i) => i / 20), 
+        Array.from({ length: 21 }, (_, i) => (i % 2 === 0 ? 0 : 20))
+    );
+    const legRunAbove = useTransform(scrollYProgress, 
+        Array.from({ length: 21 }, (_, i) => i / 20), 
+        Array.from({ length: 21 }, (_, i) => (i % 2 === 0 ? -20 : 20))
+    );
+    const legRunBehind = useTransform(scrollYProgress, 
+        Array.from({ length: 21 }, (_, i) => i / 20), 
+        Array.from({ length: 21 }, (_, i) => (i % 2 === 0 ? 0 : -40))
+    );    
 
     return (
-        <>
-            <motion.div
-                className={styles.runningManContainer}
-                style={{
-                    x: scrollTransformX,
-                }}
-                ref={refGuy}
-            >
+        <motion.div
+            className={styles.runningManContainer}
+            style={{
+                x: scrollTransformX,
+            }}
+            ref={refGuy}
+        >
+            <div className={styles.runningMan}>
                 <img src={bodyImage} alt="Corps d'un l'homme qui court" className={styles.body} />
-                <img src={leftLegImage} alt="Jambe gauche" className={styles.leftLeg} />
-                <img src={leftArmImage} alt="Bras gauche" className={styles.leftArm} />
-                <img src={rightLegImage} alt="Jambe droite" className={styles.rightLeg} />
-                <img src={rightArmImage} alt="Bras droit" className={styles.rightArm} />
-            </motion.div>
-        </>
+                <motion.img
+                    src={leftArmImage}
+                    alt="Bras gauche"
+                    className={styles.leftArm}
+                    style={{ rotate: armSwing }}
+                />
+                <motion.img
+                    src={rightArmImage}
+                    alt="Bras droit"
+                    className={styles.rightArm}
+                    style={{ rotate: armSwing }}
+                />
+                <motion.img
+                    src={leftLegImage}
+                    alt="Jambe gauche"
+                    className={styles.leftLeg}
+                    style={{ rotate: legRunBehind }}
+                />
+                <motion.img
+                    src={rightLegImage}
+                    alt="Jambe droite"
+                    className={styles.rightLeg}
+                    style={{ rotate: legRunAbove }}
+                />
+            </div>
+        </motion.div>
     );
 };
 
